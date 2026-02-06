50815b06147ce70eea4a3221173deb6c8273090c
Lucca-Fernandes <lfdc212@gmail.com>
2026-02-06 10:48:48 -0300

    FIX autorization

--- backend/server.js
+++ backend/server.js
@@ -14,15 +14,14 @@ const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, 
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000, 
-  greetingTimeout: 10000,
   tls: {
+    ciphers: 'SSLv3',
     rejectUnauthorized: false 
   }
 });
